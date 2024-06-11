3ed04804c3cb51d067d3b43b57c75ecaa0b87f6a
LongKhanhDuong <longkhanh_Duong@hotmail.de>
2024-06-11 14:45:37 +0200

    Update ExplainableAI.jl

--- src/ExplainableAI.jl
+++ src/ExplainableAI.jl
@@ -13,10 +13,12 @@ include("bibliography.jl")
 include("input_augmentation.jl")
 include("gradient.jl")
 include("gradcam.jl")
+include("lime.jl")
 
 export Gradient, InputTimesGradient
 export NoiseAugmentation, SmoothGrad
 export InterpolationAugmentation, IntegratedGradients
 export GradCAM
+export sampling_x_dash, calc_dist, calc_weights
 
 end # module
